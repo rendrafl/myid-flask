a2313df6cf12d7714276963d8c1b1cf4c44df6ee
RendraFL <rendraf28@gmail.com>
2018-09-23 21:15:08 +0700

    push2

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('profile.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
